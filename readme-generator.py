aceb62443a552eac4d5daab8864c8de99599f414
root <josh@joshbegley.com>
2020-02-20 13:20:20 -0500

    automatic updates

--- readme-generator.py
+++ readme-generator.py
@@ -2,8 +2,6 @@ import urllib2
 from bs4 import BeautifulSoup
 import csv
 
-print "Headline | URL"
-print "------ | ---"
 url = ("https://www.nytimes.com/search?query=&sort=newest")
 
 page = urllib2.urlopen(url)
@@ -14,4 +12,6 @@ for a in soup.find_all("a",href=True):
 		if "cooking.nytimes.com" in a["href"]:
 			pass
 		else:
-			print (a.h4.text.encode("utf-8")) + " | " + ("https://nytimes.com{}".format(a.attrs['href']))
+			print "###" + (a.h4.text.encode("utf-8"))
+			print "`" + (a.p.text.encode("utf-8")) + "`"
+			print ("https://nytimes.com{}".format(a.attrs['href'])) + "\n"
