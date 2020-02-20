542b4fa431818fe7b6b541bff7b08020941b1b37
root <josh@joshbegley.com>
2020-02-20 11:38:01 -0500

    automatic updates

--- nyt-latest.py
+++ nyt-latest.py
@@ -2,7 +2,7 @@ import urllib2
 from bs4 import BeautifulSoup
 import csv
 
-print "<html><head><style>h4{margin:0;font-weight:300;}</style></head>"
+print "<html><head><style>html,body,p,h4{margin:0;font-family:avenir;font-weight:normal;font-size:1em;}a{font-size:0.8em;}</style></head>"
 url = ("https://www.nytimes.com/search?query=&sort=newest")
 
 page = urllib2.urlopen(url)
@@ -10,9 +10,10 @@ soup = BeautifulSoup(page, 'html.parser')
 
 for a in soup.find_all("a",href=True):
 	if "ResultPosition=" in a["href"]:
-		print (a.h4)
-		print ("<a href=\"https://nytimes.com{}".format(a.attrs['href']))
-		print "\">"
-		print "https://nytimes.com{}".format(a.attrs['href'])
-		print "</a><br><br>"
+		if "cooking.nytimes.com" in a["href"]:
+			print("<br>")
+		else:
+
+			print (a.h4)
+			print ("<a href=\"https://nytimes.com{}".format(a.attrs['href']) + "\">" + "https://nytimes.com{}".format(a.attrs['href']) + "</a><br><br>")
 print "</html>"
