014a2800ca0e2e9e9e1dcc5a3cd23b5aeb3cfb1b
root <josh@joshbegley.com>
2024-02-23 15:11:02 -0500

    automatic updates

--- nyt-latest.py
+++ nyt-latest.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import csv
 
 print ("<html><head><style>html,body,p,h4{margin:0;font-family:avenir;font-weight:normal;font-size:1em;line-height:1.4em}a{font-size:0.8em;}h4{font-size:1.6em;}body{margin-left:20px}</style></head>")
-url = ("https://www.nytimes.com/search?query=+")
+url = ("https://www.nytimes.com/search?query=&sort=newest")
 
 page = urllib2.urlopen(url)
 soup = BeautifulSoup(page, 'html.parser')
