7cb450fc321ca7f31019d31c37a289c5d28a85c1
root <josh@joshbegley.com>
2024-03-05 12:48:20 -0500

    automatic updates

--- readme-generator.py
+++ readme-generator.py
@@ -12,6 +12,7 @@ for a in soup.find_all("a",href=True):
 		if "cooking.nytimes.com" in a["href"]:
 			pass
 		else:
+			cleaned_href = a["href"].split("?")[0]
 			print ("**" + (a.h4.text.encode("utf-8")) + "**" + "\\")
 			print ("`" + (a.p.text.encode("utf-8")) + "`" + "\\")
-			print ("https://nytimes.com{}".format(a.attrs['href'])) + "\n"
+			print ("https://nytimes.com{}" + cleaned_href + "\n"
