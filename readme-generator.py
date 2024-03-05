14ea8c2365426d06002bd221c80f11c115b0c610
root <josh@joshbegley.com>
2024-03-05 12:36:22 -0500

    automatic updates

--- readme-generator.py
+++ readme-generator.py
@@ -1,15 +1,17 @@
-from urllib.request import urlopen
+import urllib2
 from bs4 import BeautifulSoup
+import csv
 
-url = "https://www.nytimes.com/search?query=a&sort=newest"
+url = ("https://www.nytimes.com/search?query=a&sort=newest")
 
-page = urlopen(url)
+page = urllib2.urlopen(url)
 soup = BeautifulSoup(page, 'html.parser')
 
-for a in soup.find_all("a", href=True):
-    if "ResultPosition=" in a["href"]:
-        if "cooking.nytimes.com" not in a["href"]:
-            cleaned_href = a["href"].split("?")[0]
-            print("**" + a.h4.text + "**" + "\\")
-            print("`" + a.p.text + "`" + "\\")
-            print("https://nytimes.com" + cleaned_href + "\n")
+for a in soup.find_all("a",href=True):
+	if "ResultPosition=" in a["href"]:
+		if "cooking.nytimes.com" in a["href"]:
+			pass
+		else:
+			print ("**" + (a.h4.text.encode("utf-8")) + "**" + "\\")
+			print ("`" + (a.p.text.encode("utf-8")) + "`" + "\\")
+			print ("https://nytimes.com{}".format(a.attrs['href'])) + "\n"
