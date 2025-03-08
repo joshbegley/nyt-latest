dcc7b37003dae7db133f5da74fa59dbb9276fdcf
root <josh@joshbegley.com>
2025-03-08 15:38:18 +0000

    automatic updates

--- nyt-latest.py
+++ nyt-latest.py
@@ -1,12 +1,12 @@
-import urllib2
+import requests
 from bs4 import BeautifulSoup
 import csv
 
 print ("<html><head><style>html,body,p,h4{margin:0;font-family:avenir;font-weight:normal;font-size:1em;line-height:1.4em}a{font-size:0.8em;}h4{font-size:1.6em;}body{margin-left:20px}</style></head><body>")
 url = ("https://www.nytimes.com/search?query=&sort=newest")
 
-page = urllib2.urlopen(url)
-soup = BeautifulSoup(page, 'html.parser')
+page = requests.get(url)
+soup = BeautifulSoup(page.text, 'html.parser')
 
 for a in soup.find_all("a",href=True):
 	if "ResultPosition=" in a["href"]:
