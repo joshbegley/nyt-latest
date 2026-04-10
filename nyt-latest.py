b31e9d65499f723bff1f70ece9c6e5c36a473b12
root <josh@joshbegley.com>
2026-04-10 00:53:54 +0000

    automatic updates

--- nyt-latest.py
+++ nyt-latest.py
@@ -1,19 +1,22 @@
+import os
 import requests
-from bs4 import BeautifulSoup
-import csv
 
-print ("<html><head><style>html,body,p,h4{margin:0;font-family:avenir;font-weight:normal;font-size:1em;line-height:1.4em}a{font-size:0.8em;}h4{font-size:1.6em;}body{margin-left:20px}</style></head><body>")
-url = ("https://www.nytimes.com/search?query=&sort=newest")
+API_KEY = os.environ.get("NYT_API_KEY", "")
 
-page = requests.get(url)
-soup = BeautifulSoup(page.text, 'html.parser')
+url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
+params = {"sort": "newest", "api-key": API_KEY}
 
-for a in soup.find_all("a",href=True):
-	if "ResultPosition=" in a["href"]:
-		if "cooking.nytimes.com" in a["href"]:
-			print("<br>")
-		else:
-			cleaned_href = a["href"].split("?")[0]
-			print (a.h4)
-			print ("<a href=\"https://nytimes.com" + cleaned_href + "\">" + "https://nytimes.com" + cleaned_href + "</a><br><br>")
-print ("</body></html>")
+r = requests.get(url, params=params)
+data = r.json()
+
+print('<html><head><style>html,body,p,h4{margin:0;font-family:avenir;font-weight:normal;font-size:1em;line-height:1.4em}a{font-size:0.8em;}h4{font-size:1.6em;}body{margin-left:20px}</style></head><body>')
+
+for doc in data["response"]["docs"]:
+    if "cooking.nytimes.com" in doc["web_url"]:
+        continue
+    headline = doc["headline"]["main"]
+    web_url = doc["web_url"]
+    print(f"<h4>{headline}</h4>")
+    print(f'<a href="{web_url}">{web_url}</a><br><br>')
+
+print("</body></html>")
